b724a9ec4c067eb223f7e25f6d4205ce536eda41
velar <velar@DESKTOP-1RNF8E2>
2022-08-05 23:19:09 -0500

    Finish date class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptsPOO;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("=============");
+
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(2022, 8, 25));
+    Console.WriteLine(new Date(2022, 8, 5));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
